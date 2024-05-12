3d3cd7845cebd4d3ed08381ae89ef630d10ede67
codeit <mdy3722@codeit.kr>
2024-05-12 20:45:27 +0900

    Add square function

--- calculate.py
+++ calculate.py
@@ -2,13 +2,12 @@ def add(a, b):
     return a+b
 def sub(a, b):
     return a-b
-<<<<<<< HEAD
 
 def divide(a, b):
     return a/b
 
 def mul_free(a, b):
-=======
-def mult(a, b):
->>>>>>> 99b471aeed70f08cefa6a863e29b656306e6686d
     return a*b
+
+def square(a):
+    return a*a
